5f2d74f492b45c445ffa01bee4c800465d110489
Chetan kanade <chetankanade8@gmail.com>
2022-02-26 10:08:03 +0530

    My Massage App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Message from './component/Message';
+
+
+function App() {
+
+  return (
+    <>
+      <div className='app-div'>
+        <div className='m-div'>
+          <Message />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
